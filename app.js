da938c48a25364160299ad2029c96a005f8edf68
Paula-Petra <paulapetrai@gmail.com>
2023-04-12 17:34:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//defining variables
 const pl1Btn = document.querySelector('#pl1Btn');
 const pl2Btn = document.querySelector('#pl2Btn');
 const resetBtn = document.querySelector('#resetBtn');
@@ -6,6 +7,8 @@ const player2 = document.querySelector('#player2');
 const zero = 0;
 const maxScore = document.querySelector('#maxScore');
 
+
+//onclick function for +1 Player1 Button
 pl1Btn.addEventListener('click', (e) => {
     e.preventDefault();
     const score1 = parseInt(player1.innerText) + 1;
@@ -13,6 +16,7 @@ pl1Btn.addEventListener('click', (e) => {
     win();
 });
 
+//onclick function for +1 Player2 Button
 pl2Btn.addEventListener('click', (e) => {
     e.preventDefault();
     const score2 = parseInt(player2.innerText) + 1;
@@ -20,11 +24,18 @@ pl2Btn.addEventListener('click', (e) => {
     win();
 });
 
+//onclick function for Reset Button
 resetBtn.addEventListener('click', (e) => {
     e.preventDefault();
     reset();  
 });
 
+//onchange function for select dropdown
+maxScore.addEventListener('change', () => {
+    reset();
+})
+
+//function for gameo over
 function win(){
     if(player1.innerText == maxScore.value || player2.innerText == maxScore.value){
         pl1Btn.disabled = true;
@@ -32,18 +43,21 @@ function win(){
         pl1Btn.style.backgroundColor = "rgba(76, 237, 119, 0.6)";
         pl2Btn.style.backgroundColor = "rgba(50, 203, 237, 0.6)";
         if(player1.innerText == maxScore.value){
-            player1.style.color = "green";
-            player2.style.color = "red";
+            player1.classList.add('winner');
+            player2.classList.add('loser');
         } else{
-            player1.style.color = "red";
-            player2.style.color = "green";
+            player1.classList.add('loser');
+            player2.classList.add('winner');
         }
     }
 };
 
+//reset function
 function reset(){
     player1.innerText = zero;
     player2.innerText = zero;
+    player1.classList.remove('loser', 'winner');
+    player2.classList.remove('winner', 'loser');
     pl1Btn.disabled = false;
     pl2Btn.disabled = false;
     player1.style.color = ""
